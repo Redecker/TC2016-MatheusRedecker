3ebadf6b919a17d15b9c63249fad47ec2f4e3588
Matheus Redecker <matheusredecker@gmail.com>
2016-10-13 16:44:49 -0300

    started to make a new domain

--- code/test/criaPlano.sh
+++ code/test/criaPlano.sh
@@ -2,9 +2,9 @@
 
 export CLASSPATH=$CLASSPATH:/home/redecker/Documents/TC2016-MatheusRedecker/code/lib/antlr.jar:/home/redecker/Documents/TC2016-MatheusRedecker/code/lib/JSHOP2.jar:.
 
-java JSHOP2.InternalDomain ahtnTeste
+java JSHOP2.InternalDomain domain
 
-java JSHOP2.InternalDomain -r2 problem
+java JSHOP2.InternalDomain -ra problem
 
 javac gui.java
 
--- code/test/gui.java
+++ code/test/gui.java
@@ -11,21 +11,7 @@ import java.io.OutputStreamWriter;
 
 public class gui{
 	public static void main(String[] args) {
-		
-		try (BufferedWriter buffWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("plano.txt"), "UTF-8"))) {
-	        buffWriter.write(problem.getPlans().toString());    
-	        buffWriter.close();
-		} catch (Exception e) {
-		   	e.printStackTrace();
-		}
-
-		new JSHOP2GUI();
-		//LinkedList<Plan> llp = problem.getPlans();
-		//LinkedList<Predicate> llpre = llp.peek().getOps();
-		//System.out.println(llpre.peek());
-		//System.out.println(llp.size());
-		//System.out.println(llp);
-		//System.out.println(problem.getPlans());
+		System.out.System.out.print(problem.getPlans());
 		//new JSHOP2GUI();
 	} 
 }
