64929a66703919c101529c77dc6304f55e03a184
Matheus Redecker <matheusredecker@gmail.com>
2016-11-30 13:35:59 -0200

    little changes

--- code/src/tests/GameVisualSimulationTest.java
+++ code/src/tests/GameVisualSimulationTest.java
@@ -35,9 +35,9 @@ public class GameVisualSimulationTest {
     public static void main(String args[]) throws Exception {
         UnitTypeTable utt = new UnitTypeTable();
         UnitTypeTable utt2 = new UnitTypeTable();
-        PhysicalGameState pgs = PhysicalGameState.load("maps/basesWorkers16x16.xml", utt);
+        //PhysicalGameState pgs = PhysicalGameState.load("maps/basesWorkers16x16.xml", utt);
         //PhysicalGameState pgs = PhysicalGameState.load("maps/basesWorkers8x8Obstacle.xml", utt);
-        //PhysicalGameState pgs = PhysicalGameState.load("maps/basesWorkersBarracks8x8.xml", utt);
+        PhysicalGameState pgs = PhysicalGameState.load("maps/basesWorkersBarracks8x8.xml", utt);
 
         GameState gs = new GameState(pgs, utt);
         int MAXCYCLES = 5000;
@@ -56,17 +56,17 @@ public class GameVisualSimulationTest {
         //AI ai1 = new LightRush(utt, new BFSPathFinding());
         //AI ai1 = new WorkerRush(utt, new BFSPathFinding());
         //AI ai1 = new MonteCarlo(100, -1, 100, new RandomBiasedAI(), new SimpleEvaluationFunction());
-        //AI ai1 = new IDRTMinimax(100, new SimpleEvaluationFunction());
-        AI ai1 = new PortfolioAI(new AI[]{new WorkerRush(utt), new LightRush(utt), new RangedRush(utt), new RandomBiasedAI()}, new boolean[]{true,true,true,false}, 100, -1, 400, new SimpleEvaluationFunction());
+        AI ai1 = new IDRTMinimax(100, new SimpleEvaluationFunction());
+        //AI ai1 = new PortfolioAI(new AI[]{new WorkerRush(utt), new LightRush(utt), new RangedRush(utt), new RandomBiasedAI()}, new boolean[]{true,true,true,false}, 100, -1, 400, new SimpleEvaluationFunction());
         
 
-        //myAHTN ai2 = new myAHTN(utt, new BFSPathFinding());
+        myAHTN ai2 = new myAHTN(utt, new BFSPathFinding());
         //AI ai2 = new RandomAI();
         //AI ai2 = new RandomBiasedAI();
         //AI ai2 = new RangedRush(utt, new BFSPathFinding());
         //AI ai2 = new HeavyRush(utt, new BFSPathFinding());  
         //AI ai2 = new LightRush(utt, new BFSPathFinding());
-        AI ai2 = new WorkerRush(utt, new BFSPathFinding());
+        //AI ai2 = new WorkerRush(utt, new BFSPathFinding());
         //AI ai2 = new MonteCarlo(100, -1, 100, new RandomBiasedAI(), new SimpleEvaluationFunction());
         //AI ai2 = new IDRTMinimax(100, new SimpleEvaluationFunction());
         //AI ai2 = new PortfolioAI(new AI[]{new WorkerRush(utt), new LightRush(utt), new RangedRush(utt), new RandomBiasedAI()}, new boolean[]{true,true,true,false}, 100, -1, 400, new SimpleEvaluationFunction());
